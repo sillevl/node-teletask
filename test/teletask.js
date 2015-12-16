c4f610623e14f97beceaecc0564de77125cd0c49
Sille Van Landschoot <info@sillevl.be>
2015-12-16 09:41:38 +0100

    add report event test

--- test/teletask.js
+++ test/teletask.js
@@ -1,6 +1,6 @@
 var Teletask  = require('../teletask'),
 		net       = require('net'),
-		expect    = require('chai').expect
+		expect    = require('chai').expect;
 
 describe('Teletask', function(){
 
@@ -13,15 +13,28 @@ describe('Teletask', function(){
 	beforeEach(function(done){
 		teletask = new Teletask.connect(HOST,PORT);
 		server = net.createServer().listen(PORT,done);
-	})
+	});
 
 	afterEach(function() {
-		server.close()
-	})
+		server.close();
+	});
 
-	it('create teletask server with default host and port')
-	it('create teletask server with default host and custom port')
-	it('create teletask server with custom host and default port')
-	it('create teletask server with custom host and custom port')
+	it('create teletask server with default host and port');
+	it('create teletask server with default host and custom port');
+	it('create teletask server with custom host and default port');
+	it('create teletask server with custom host and custom port');
 
-})
+	it('should receive events', function(done){
+		teletask.on("report", function(report){
+			expect(report.fnc).to.be.equal(Teletask.functions.getKey(Teletask.functions.relay));
+			expect(report.number).to.be.equal(21);
+			done();
+		});
+		teletask.get(Teletask.functions.relay, 21);
+		server.once("connection", function(sock){
+			sock.once("data", function(data) {
+				sock.write(new Buffer([2, 9, 0x10, 1, 1, 0, 21, 0, 255, 0x31]));
+			});
+		});
+	});
+});
